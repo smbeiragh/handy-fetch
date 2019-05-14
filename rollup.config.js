2becc92fbe520a376dbe78cbe3819662f441ef49
sajjad m.beiraghdar <smbeiragh@gmail.com>
2019-05-14 14:28:43 +0430

    update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: 'src/index.js',
@@ -14,6 +14,6 @@ export default {
     babel({
       runtimeHelpers: true,
     }),
-    uglify(),
+    terser(),
   ],
 };
